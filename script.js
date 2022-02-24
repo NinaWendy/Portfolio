9b17eeb63891b2be8d10eedf134839dc8e07d084
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-02-24 16:41:04 +0300

    feature:add functionality to education section's buttons

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 //education section
-// var acc = document.getElementsByClassName("accord");
-// var i;
+var acc = document.getElementsByClassName("accord");
+var i;
 
-// for (i=0;i< acc.length;i++){
-//     acc[i].addEventListener("click", function(){
-//       this.classList.toggle("active");
-//     var card = this.nextElementSibling;
-//     if (card.style.display ==="block"){
-//       card.style.display ="none"
-//     }else{
-//       card.style.display="block";
-//     }
-//   });
-// }
+for (i=0;i< acc.length;i++){
+    acc[i].addEventListener("click", function(){
+      this.classList.toggle("active");
+    var card = this.nextElementSibling;
+    if (card.style.display ==="block"){
+      card.style.display ="none"
+    }else{
+      card.style.display="block";
+    }
+  });
+}
 
