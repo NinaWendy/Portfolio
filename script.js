62204a5831c48222b5f417496020697f408ff487
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-02-24 16:38:35 +0300

    feature: add education section

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function closeNav() {
   document.getElementById("nav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+//education section
+// var acc = document.getElementsByClassName("accord");
+// var i;
+
+// for (i=0;i< acc.length;i++){
+//     acc[i].addEventListener("click", function(){
+//       this.classList.toggle("active");
+//     var card = this.nextElementSibling;
+//     if (card.style.display ==="block"){
+//       card.style.display ="none"
+//     }else{
+//       card.style.display="block";
+//     }
+//   });
+// }
+
